6244eb8ff3919bf576496c8eba6981732537bd62
mariapaulavera <mariapaulaveram@gmail.com>
2025-09-29 07:37:02 -0300

    comente StrictMode en main.jsx para DocenaEmpanadasModal +-1

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  //<StrictMode>  //comentado para que funcione agregar empanadas de a 1 en DocenaEmpanadasModal
     <App />
-  </StrictMode>,
+  //</StrictMode>,
 )
